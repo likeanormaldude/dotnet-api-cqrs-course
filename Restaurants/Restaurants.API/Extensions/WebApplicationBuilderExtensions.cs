41e23558195e71173aa68a57fc40edb0ffcf92d8
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-14 17:28:52 -0400

    Extract presentation layer services

---
+++ Restaurants/Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.OpenApi.Models;
+using Restaurants.API.Behaviors;
+using Restaurants.API.Middlewares;
+using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+using Serilog;
+
+namespace Restaurants.API.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    // Add services to the container.
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+            cfg.RegisterServicesFromAssembly(typeof(GetAllRestaurantsQueryHandler).Assembly);
+            cfg.LicenseKey = builder.Configuration["MediatR:LicenseKey"];
+        });
+
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(cfg =>
+        {
+            string securityDefinitionId = "bearerAuth";
+
+            cfg.AddSecurityDefinition(
+                securityDefinitionId,
+                new OpenApiSecurityScheme() { Type = SecuritySchemeType.Http, Scheme = "Bearer" }
+            );
+
+            cfg.AddSecurityRequirement(
+                new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = securityDefinitionId,
+                            },
+                        },
+                        []
+                    },
+                }
+            );
+        });
+
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+        builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+        builder.Services.AddScoped<ValidationExceptionMiddleware>();
+        builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<RestaurantsDbContext>();
+        builder.Host.UseSerilog((context, cfg) => cfg.ReadFrom.Configuration(context.Configuration));
+    }
+}
--- Restaurants/Restaurants.API/Program.cs
+++ Restaurants/Restaurants.API/Program.cs
@@ -1,66 +1,18 @@
-using MediatR;
-using Microsoft.OpenApi.Models;
-using Restaurants.API.Behaviors;
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
-using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
 using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
-using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddMediatR(cfg =>
-{
-    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
-    cfg.RegisterServicesFromAssembly(typeof(GetAllRestaurantsQueryHandler).Assembly);
-    cfg.LicenseKey = builder.Configuration["MediatR:LicenseKey"];
-});
-
-builder.Services.AddSwaggerGen(cfg =>
-{
-    string securityDefinitionId = "bearerAuth";
-
-    cfg.AddSecurityDefinition(
-        securityDefinitionId,
-        new OpenApiSecurityScheme() { Type = SecuritySchemeType.Http, Scheme = "Bearer" }
-    );
-
-    cfg.AddSecurityRequirement(
-        new OpenApiSecurityRequirement()
-        {
-            {
-                new OpenApiSecurityScheme()
-                {
-                    Reference = new OpenApiReference()
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = securityDefinitionId,
-                    },
-                },
-                []
-            },
-        }
-    );
-});
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-builder.Services.AddScoped<ValidationExceptionMiddleware>();
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-builder.Services.AddIdentityApiEndpoints<User>().AddEntityFrameworkStores<RestaurantsDbContext>();
-
-builder.Services.AddControllers();
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Host.UseSerilog((context, cfg) => cfg.ReadFrom.Configuration(context.Configuration));
-
 var app = builder.Build();
-
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
 await seeder.Seed();
