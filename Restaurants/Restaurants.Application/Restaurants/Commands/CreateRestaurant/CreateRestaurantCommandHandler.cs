1df64250ec38e4e388211b6bd795a1f539290e81
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-18 23:23:06 -0400

    Add Owner to Restaurant prop

--- Restaurants/Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants/Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MapsterMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
@@ -9,13 +10,23 @@ namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 public class CreateRestaurantCommandHandler(
     ILogger<CreateRestaurantCommandHandler> logger,
     IMapper mapper,
-    IRestaurantsRepository restaurantsRepository
+    IRestaurantsRepository restaurantsRepository,
+    IUserContext userContext
 ) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Creating a new restaurant {@Restaurant}", request);
+        var currentUser = userContext.GetCurrentUser();
+
+        logger.LogInformation(
+            "{Username} [{UserId}] is creating a new restaurant {@Restaurant}",
+            currentUser.Email,
+            currentUser.Id,
+            request
+        );
         Restaurant restaurant = mapper.Map<Restaurant>(request)!;
+        restaurant.OwnerId = currentUser.Id;
+
         int id = await restaurantsRepository.Create(restaurant);
         return id;
     }
