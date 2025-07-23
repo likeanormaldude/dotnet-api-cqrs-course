1070ddcd8709a685d14fe4523d82a1ea80701650
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-22 21:09:12 -0400

    XUnit 1st test + FluentAssertions

---
+++ Restaurants/Restaurants.Application.Tests/Users/CurrentUserTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Restaurants.Domain.Constants;
+using Xunit;
+
+namespace Restaurants.Application.Users.Tests;
+
+public class CurrentUserTests
+{
+    // Suggested naming convention for test methods: // [MethodName]_[Condition]_[ExpectedResult]
+    // Suggested steps for writing tests: Arrange, Act, Assert
+
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData("admin")]
+    [InlineData("ADMIN")]
+    [InlineData(UserRoles.User)]
+    public void IsInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
+    {
+        // Arrange
+        var currentUser = new CurrentUser("1", "mail@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // Act
+        bool isInRole = currentUser.IsInRole(roleName);
+
+        // Assert
+        isInRole.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+    {
+        // Arrange
+        var currentUser = new CurrentUser("1", "mail@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // Act
+        bool isInRole = currentUser.IsInRole(UserRoles.Owner);
+
+        // Assert
+        isInRole.Should().BeFalse();
+    }
+
+    [Fact()]
+    public void IsInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+    {
+        string dummyRoleShouldBeIgnored = "dummyRole";
+        // Arrange
+        var currentUser = new CurrentUser("1", "mail@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // Act
+        bool isInRole = currentUser.IsInRole(dummyRoleShouldBeIgnored);
+
+        // Assert
+        isInRole.Should().BeFalse();
+    }
+}
