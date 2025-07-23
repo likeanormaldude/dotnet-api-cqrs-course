38eab85ce80d3de8659d59e3caf6a704e55f90f9
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-23 13:07:26 -0400

    Add Validator tests

---
+++ Restaurants/Restaurants.Application.Tests/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant.Tests;
+
+public class CreateRestaurantCommandValidatorTests
+{
+    [Fact()]
+    public void Validator_ForValidCommand_ShouldNotReturnValidationErrors()
+    {
+        #region Arrange
+        CreateRestaurantCommand command = new()
+        {
+            Name = "Test",
+            Description = "A great place to eat.",
+            Category = "Italian",
+            PostalCode = "H3Y 3F2",
+            ContactEmail = "test@test.com",
+        };
+
+        CreateRestaurantCommandValidator validator = new();
+        #endregion
+        #region Act
+        var validationResult = validator.TestValidate(command);
+        #endregion
+        #region Assert
+        validationResult.ShouldNotHaveAnyValidationErrors();
+        #endregion
+    }
+
+    [Fact()]
+    public void Validator_ForInvalidCommand_ShouldReturnValidationErrors()
+    {
+        #region Arrange
+        CreateRestaurantCommand command = new()
+        {
+            Name = "Te",
+            Description = "A great place to eat.",
+            Category = "",
+            PostalCode = "H3Y 3F2",
+            ContactEmail = "test@",
+        };
+
+        CreateRestaurantCommandValidator validator = new();
+        #endregion
+
+        #region Act
+        var validationResult = validator.TestValidate(command);
+        #endregion
+
+        #region Assert
+        Console.WriteLine(validationResult.ToString());
+        //validationResult.ShouldHaveValidationErrors();
+        validationResult.ShouldHaveValidationErrorFor(dto => dto.Name);
+        validationResult.ShouldHaveValidationErrorFor(dto => dto.Category);
+        validationResult.ShouldHaveValidationErrorFor(dto => dto.ContactEmail);
+        #endregion
+    }
+}
--- Restaurants/Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
+++ Restaurants/Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandValidator.cs
@@ -12,7 +12,10 @@ public class CreateRestaurantCommandValidator : AbstractValidator<CreateRestaura
 
         RuleFor(dto => dto.Category).NotEmpty().WithMessage("Category is required.");
 
-        RuleFor(dto => dto.ContactEmail).EmailAddress().WithMessage("Please provide a valid email address");
+        RuleFor(dto => dto.ContactEmail)
+            .EmailAddress()
+            .When(dto => !string.IsNullOrEmpty(dto.ContactEmail))
+            .WithMessage("Please provide a valid email address");
 
         RuleFor(dto => dto.PostalCode)
             .Matches(@"^((\S{3})(\s{1})?(\S{3}))$")
