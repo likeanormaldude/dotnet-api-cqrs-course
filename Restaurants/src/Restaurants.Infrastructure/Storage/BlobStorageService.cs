fbcca7f1b7965c840ed3e212c0578b79b6764756
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-08-15 19:23:50 -0400

    Add endpoint to update logos to Azure BlobStorage Acccount
    Squashed commits:
    - Fix custom configuration
    - Add owner to UpdateRestaurantCommand

---
+++ Restaurants/src/Restaurants.Infrastructure/Storage/BlobStorageService.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Interfaces;
+using Restaurants.Infrastructure.Configuration;
+
+namespace Restaurants.Infrastructure.Storage;
+
+public class BlobStorageService(IOptions<BlobStorageSettings> blobStorageSettingsOptions) : IBlobStorageService
+{
+    public readonly BlobStorageSettings _blobStorageSettings = blobStorageSettingsOptions.Value;
+
+    public async Task<string> UploadToBlobAsync(Stream data, string fileName)
+    {
+        BlobServiceClient blobServiceClient = new(_blobStorageSettings.ConnectionString);
+        var containerClient = blobServiceClient.GetBlobContainerClient(_blobStorageSettings.LogosContainerName);
+
+        var blobClient = containerClient.GetBlobClient(fileName);
+        await blobClient.UploadAsync(data);
+        return blobClient.Uri.ToString();
+    }
+}
